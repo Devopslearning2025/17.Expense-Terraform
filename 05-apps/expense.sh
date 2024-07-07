10f7a51e7f0609f58a1f838cdd7b85a66d1eda42
Ramesh <dev0pslearning2025@gmail.com>
2024-07-07 10:59:24 +0530

    jenkins

--- 05-apps/expense.sh
+++ 05-apps/expense.sh
@@ -6,5 +6,5 @@ dnf install ansible -y
 cd /tmp
 git clone https://github.com/Devopslearning2025/9.Expense-Ansible-Roles.git
 cd 9.Expense-Ansible-Roles
-ansible-playbook main.yaml -e component=backend -e login_password=ExpenseApp1
+ansible-playbook main.yaml -e component=backend -e root_password=ExpenseApp1
 ansible-playbook main.yaml -e component=frontend
